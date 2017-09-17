a25cd60fe0005c2b00d88f1c9ed095dff1197200
Gregoire Hoin <gregone@Gregoires-MacBook-Pro.local>
2017-09-16 17:44:36 -0700

    chore(data): set up fake data for exercise

--- app/routes/t-shirts.js
+++ app/routes/t-shirts.js
@@ -4,9 +4,19 @@ export default Ember.Route.extend({
   model(params) {
     return [{
       id: 1,
+      name: 'Zoey',
       colorways: ['red', 'blue', 'green']
     }, {
       id: 2,
+      name: 'Tomster',
+      colorways: ['black', 'gray']
+    },{
+      id: 3,
+      name: 'Hipster Zoey',
+      colorways: ['red', 'blue', 'green']
+    }, {
+      id: 4,
+      name: 'Hipster Tomster',
       colorways: ['black', 'gray']
     }]
   }
